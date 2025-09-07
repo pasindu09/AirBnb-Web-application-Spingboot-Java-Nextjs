87ca57a300f40713deccfa9ae01849368bfd031a
pasindu09 <cb009454@students.apiit.lk>
2025-09-07 21:06:45 +0530

    changes v1

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,5 @@ public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
     }
-    // @GetMapping("/hello")
-    // public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-    //   return String.format("Hello %s!", name);
-    // }
+   
 }
