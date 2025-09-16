2e38e1e83e0d2171810d2ebe734ed1f5c5a8e96f
pasindu09 <cb009454@students.apiit.lk>
2025-09-16 21:02:29 +0530

    "demo" changed to "backend"

--- backend/src/main/java/com/example/demo/entity/RentalImage.java
+++ backend/src/main/java/com/example/demo/entity/RentalImage.java
@@ -1,12 +1,9 @@
 package com.example.demo.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,9 +18,9 @@ public class RentalImage {
 
     private String caption;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "rental_id", nullable = false)
-    private Rental rental;
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "rental_id", nullable = false)
+    // private Rental rental;
 
     // Constructors, Getters, Setters, etc.
 }
